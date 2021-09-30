79e90081996b93f754095b8a1bd6200cbe1709d7
Bolgar <bolgar888@gmail.com>
2021-09-30 15:26:10 +0300

    Sborka

--- src/test/java/tests/demoqa/RegistrationFormTests.java
+++ src/test/java/tests/demoqa/RegistrationFormTests.java
@@ -1,13 +1,12 @@
-package guruqa.demoqa;
+package tests.demoqa;
 import com.github.javafaker.Faker;
 import org.junit.jupiter.api.Test;
-import guruqa.TestBase;
+import tests.TestBase;
 
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 import static io.qameta.allure.Allure.step;
-
 public class RegistrationFormTests {
     Faker faker = new Faker();
     String firstName = faker.name().firstName(),
