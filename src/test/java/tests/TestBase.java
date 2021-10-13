71f9718c81aefa1ce5b757108174b5407f3fe240
Bolgar <bolgar888@gmail.com>
2021-10-13 14:31:52 +0300

    remoute url

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,13 +1,22 @@
 package tests;
+
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
+import config.CredentialsConfig;
+import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.aeonbits.owner.ConfigFactory;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import helpers.Attach;
+
+import static java.lang.String.format;
+
 
 public class TestBase {
+    public static CredentialsConfig credentials =
+            ConfigFactory.create(CredentialsConfig.class);
+
     @BeforeAll
     static void setup() {
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
@@ -18,8 +27,11 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.startMaximized = true;
-        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub/";
-        //https://%s:%s@%s", login, password, selenoidUrl
+        String login = credentials.login();
+        String password = credentials.password();
+        String url = System.getProperty("REMOTE_URL");
+        Configuration.remote = format("https://%s:%s@%s", login, password, url);
+
 
     }
 
